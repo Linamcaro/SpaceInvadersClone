e2c2ad6d26f0e0ce259a27ec8b745ba80ee21a03
Linamcaro <lina.mcaro@gmail.com>
2025-07-25 15:05:32 -0500

    unity project created

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public enum GameState
+    {
+        Countdown,
+        Playing,
+        Lose,
+        Victory
+    }
+
+
+}
